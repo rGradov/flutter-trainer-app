06046be4080af5b46cb896f657c5c0868e73571d
rGradov <VladRudenualu@yandex.by>
2021-04-01 10:56:19 +0300

    hamb

--- lib/shared/hamburgermenu.dart
+++ lib/shared/hamburgermenu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:workout_app/const/const.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:workout_app/route/routerName.dart';
 import 'package:workout_app/screens/auth/authscreen.dart';
 
 class HamburgerMenu extends StatelessWidget {
@@ -12,34 +13,36 @@ class HamburgerMenu extends StatelessWidget {
         child: ListView(
           // padding: EdgeInsets.zero,
           children: <Widget>[
-            // ListTile(
-            //   leading: Icon(
-            //     Icons.exit_to_app,
-            //     color: MainColor,
-            //   ),
-            //   title: Text(
-            //     'Logout',
-            //     style: TextStyle(color: Colors.white),
-            //   ),
-            //   onTap: () => {Navigator.of(context).pop()},
-            // ),
             ListTile(
               leading: Icon(
-                Icons.account_circle,
+                Icons.exit_to_app,
                 color: MainColor,
               ),
               title: Text(
-                "login-btn-text".tr().toString(),
+                'Logout',
                 style: TextStyle(color: Colors.white),
               ),
               onTap: () => {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AuthScreen(),
-                    ))
+                 Navigator.pushNamedAndRemoveUntil(context, LoginRoute, (r) => false)
               },
-            )
+            ),
+            // ListTile(
+            //   leading: Icon(
+            //     Icons.account_circle,
+            //     color: MainColor,
+            //   ),
+            //   title: Text(
+            //     "login-btn-text".tr().toString(),
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   onTap: () => {
+            //     Navigator.push(
+            //         context,
+            //         MaterialPageRoute(
+            //           builder: (context) => AuthScreen(),
+            //         ))
+            //   },
+            // )
           ],
         ),
       ),
